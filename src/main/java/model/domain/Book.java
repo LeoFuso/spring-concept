d64a4d2d9d2d38dfc53440055cb4a6afcc23d825
LeoFuso <leonardofusonuzzo@gmail.com>
2018-05-24 20:14:47 -0300

    The other side of the @ManyToMany mapping of the last commit

--- src/main/java/model/domain/Book.java
+++ src/main/java/model/domain/Book.java
@@ -3,11 +3,21 @@ package model.domain;
 import model.Persisted;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import java.util.Set;
 
 @Entity
 public class Book extends Persisted {
 
     private String title;
     private String description;
+    private String isbn;
+
+    /**
+     * @see model.domain.Author#books
+     */
+    @ManyToMany(mappedBy = "books")
+    private Set<Author> authors;
+
 
 }
