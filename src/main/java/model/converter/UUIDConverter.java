6fbc5bc07eb1fb878fdea6f161829dadf11ca4e0
LeoFuso <leonardofusonuzzo@gmail.com>
2018-05-25 10:42:03 -0300

    Option usage

--- src/main/java/model/converter/UUIDConverter.java
+++ src/main/java/model/converter/UUIDConverter.java
@@ -2,6 +2,7 @@ package model.converter;
 
 import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
+import java.util.Optional;
 import java.util.UUID;
 
 @Converter(autoApply = true)
@@ -9,11 +10,15 @@ public class UUIDConverter implements AttributeConverter<UUID, String> {
 
     @Override
     public String convertToDatabaseColumn(UUID attribute) {
-        return attribute.toString();
+        return Optional.ofNullable(attribute)
+                .map(UUID::toString)
+                .orElse(null);
     }
 
     @Override
     public UUID convertToEntityAttribute(String dbData) {
-        return UUID.fromString(dbData);
+        return Optional.ofNullable(dbData)
+                .map(UUID::fromString)
+                .orElse(null);
     }
 }
