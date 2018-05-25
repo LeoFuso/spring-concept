242cc09a43a51822269c18a939b8a70811e58f6c
LeoFuso <leonardofusonuzzo@gmail.com>
2018-05-25 10:58:50 -0300

    Address Interface

---
+++ src/main/java/model/address/Address.java
@@ -0,0 +1,14 @@
+package model.address;
+
+/**
+ * <p>Created by <a href="https://github.com/LeoFuso">Leonardo Fuso</a> on <i>25/05/2018</i></p>
+ * <p>
+ * Created in order to provide a unified interface for the various types of address that an entity may have,
+ * and their respective behaviors.
+ * </p>
+ */
+public interface Address {
+
+    boolean validate();
+
+}
