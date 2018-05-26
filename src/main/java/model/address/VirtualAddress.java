bc2d8721ae521c32637124277387861470ce6cc0
LeoFuso <leonardofusonuzzo@gmail.com>
2018-05-25 21:10:43 -0300

    VirtualAddress class added

---
+++ src/main/java/model/address/VirtualAddress.java
@@ -0,0 +1,33 @@
+package model.address;
+
+import lombok.Data;
+import model.Persisted;
+
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.persistence.Entity;
+
+@Data
+@Entity
+public class VirtualAddress extends Persisted implements Address {
+
+    private InternetAddress internetAddress;
+
+    /**
+     * <p>
+     * This method uses the {@link InternetAddress#validate() validate()} function
+     * to validate the {@link VirtualAddress#internetAddress internetAddress} attribute.
+     * </p>
+     *
+     * @return Either internetAddress is valid or not
+     */
+    @Override
+    public boolean validate() {
+        try {
+            this.internetAddress.validate();
+            return true;
+        } catch (AddressException ignored) {
+            return false;
+        }
+    }
+}
