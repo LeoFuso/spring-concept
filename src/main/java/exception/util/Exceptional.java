b4796bf2fe4887586b2b19152ea6fbb2923012c6
LeoFuso <leonardofusonuzzo@gmail.com>
2018-06-08 00:16:36 -0300

    javadocs added

--- src/main/java/exception/util/Exceptional.java
+++ src/main/java/exception/util/Exceptional.java
@@ -4,10 +4,10 @@ import java.util.Objects;
 import java.util.function.Consumer;
 
 /**
- * <p>Handling exceptions using Java 8 Optional Interface </p>
+ * <p>Handling exceptions using an Java Optional-like inspired Interface</p>
  *
  * @param <T> reference Type of the expected value
- * @see <a href="https://dzone.com/articles/exceptional-optional"> source code at </a>
+ * @see <a href="https://dzone.com/articles/exceptional-optional"> source at </a>
  */
 public class Exceptional<T> {
 
@@ -27,9 +27,8 @@ public class Exceptional<T> {
 	}
 
 	public void rethrow() throws Exception {
-		if (exception != null) {
+		if (exception != null)
 			throw exception;
-		}
 	}
 
 	public Exception getException() {
@@ -38,6 +37,7 @@ public class Exceptional<T> {
 
 	@SuppressWarnings("unchecked")
 	private static <T> Exceptional<T> empty() {
+
 		Exceptional<T> t = (Exceptional<T>) EMPTY;
 		return t;
 	}
@@ -52,10 +52,28 @@ public class Exceptional<T> {
 		value = null;
 	}
 
+	/**
+	 * <p>
+	 * Returns a new instance of the Exceptional class built upon the value passed as parameter.
+	 * </p>
+	 *
+	 * @param value The value passed as a parameter.
+	 * @param <T>   reference Type of the expected value
+	 * @return Returns a new instance of the Exceptional class built upon the method call passed as parameter.
+	 */
 	public static <T> Exceptional<T> of(T value) {
 		return new Exceptional<>(value);
 	}
 
+	/**
+	 * <p>
+	 * Returns a new instance of the Exceptional class built upon the exception passed as parameter.
+	 * </p>
+	 *
+	 * @param exception The exception passed as a parameter.
+	 * @param <T>       reference Type of the expected value
+	 * @return Returns a new instance of the Exceptional class built upon the method call passed as parameter.
+	 */
 	public static <T> Exceptional<T> of(Exception exception) {
 		return new Exceptional<>(exception);
 	}
@@ -65,32 +83,47 @@ public class Exceptional<T> {
 	}
 
 	public T get() {
+
 		if (exception != null)
 			throw new RuntimeException(exception);
 
 		return value;
 	}
 
+	/**
+	 * <p>Returns <code>false</code> if an exception has been thrown, returns <code>true</code> otherwise</p>
+	 *
+	 * @return whether or not the value is present
+	 */
 	public boolean isPresent() {
 		return exception == null;
 	}
 
+	/**
+	 * <p>
+	 * If the value is present, it will perform a consumer-based action.
+	 * If an exception has been thrown, nothing will happen.
+	 * </p>
+	 *
+	 * @param consumer An operation that accepts a single input argument and returns no result
+	 */
 	public void ifPresent(Consumer<? super T> consumer) {
+
 		if (exception == null)
 			consumer.accept(value);
 	}
 
 	public void ifExceptionPresent(Consumer<? super Exception> consumer) {
+
 		if (exception != null)
 			consumer.accept(exception);
 	}
 
 	public void ifExceptionPresent(Class<? extends Exception> targetType,
 	                               Consumer<? super Exception> consumer) {
-		if (exception != null
-				&& targetType.isAssignableFrom(exception.getClass())) {
+
+		if (exception != null && targetType.isAssignableFrom(exception.getClass()))
 			consumer.accept(exception);
-		}
 	}
 
 	public T orElse(T other) {
