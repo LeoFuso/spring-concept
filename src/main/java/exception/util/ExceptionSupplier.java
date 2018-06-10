566bd338f0376854f7102ab1b6b11ea7821ccf71
LeoFuso <leonardofusonuzzo@gmail.com>
2018-06-10 03:55:11 -0300

    Javadoc modified

--- src/main/java/exception/util/ExceptionSupplier.java
+++ src/main/java/exception/util/ExceptionSupplier.java
@@ -3,11 +3,13 @@ package exception.util;
 /**
  * Represents a supplier of results that can throw an exception
  *
- * <p>This is a functional interface whose functional method is {@link #get()}.</p>
+ * <p>This is a <a href="package-summary.html">functional interface</a>
+ * whose functional method is {@link #get()}.
  *
  * @param <T> the type of the result supplied by this supplier
  * @param <E> the type of the exception that this supplier can throw
  * @see java.util.function.Supplier
+ * @since 1.8
  */
 @FunctionalInterface
 public interface ExceptionSupplier<T, E extends Exception> {
